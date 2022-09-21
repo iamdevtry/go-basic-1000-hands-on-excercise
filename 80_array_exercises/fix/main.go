1d22d2a2de59f58c153862d7797764ab80a4d360
phongtrollx2 <tranbinhphongcmg@gmail.com>
2022-09-21 21:37:16 +0700

    done array exercise

---
+++ 80_array_exercises/fix/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	names := [3]string{
+		"Einstein", "Shepard",
+		"Tesla",
+	}
+
+	books := [5]string{
+		"Kafka's Revenge",
+		"Stay Golden",
+		"",
+		"",
+		"",
+	}
+
+	fmt.Printf("%q\n", names)
+	fmt.Printf("%q\n", books)
+}

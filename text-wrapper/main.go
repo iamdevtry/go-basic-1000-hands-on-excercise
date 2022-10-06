c648256f4e2a418f77f03b530913f147d9eca931
phongtrollx2 <tranbinhphongcmg@gmail.com>
2022-10-06 23:25:28 +0700

    done exercise

---
+++ text-wrapper/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func main() {
+	const text = `Galaksinin Batı Sarmal Kolu'nun bir ucunda, haritası bile çıkarılmamış ücra bir köşede, gözlerden uzak, küçük ve sarı bir güneş vardır.
+	Bu güneşin yörüngesinde, kabaca yüz kırksekiz milyon kilometre uzağında, tamamıyla önemsiz ve mavi-yeşil renkli, küçük bir gezegen döner.
+	Gezegenin maymun soyundan gelen canlıları öyle ilkeldir ki dijital kol saatinin hâlâ çok etkileyici bir buluş olduğunu düşünürler.`
+
+	const maxWidth = 40
+
+	var lineWidth int
+
+	for _, r := range text {
+		fmt.Printf("%c", r)
+
+		switch lineWidth++; {
+		case lineWidth > maxWidth && r != '\n' && unicode.IsSpace(r):
+			fmt.Println()
+			fallthrough
+		case r == '\n':
+			lineWidth = 0
+		}
+	}
+
+	fmt.Println()
+}

c3a60f805aa18d7d1fce74f6ecb7db4676c1a904
iamdevtry <tranbinhphongcmg@gmail.com>
2022-11-16 13:37:45 +0700

    done lesson 168

---
+++ SECTION_13/168_functions/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	local := 10
+	show(local)
+
+	incrWrong(local)
+	fmt.Printf("main -> local = %d\n", local)
+
+	local = incr(local)
+	show(local)
+
+	local = incrBy(local, 5)
+	show(local)
+
+	_, err := incrByStr(local, "TWO")
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+	}
+
+	local, _ = incrByStr(local, "2")
+	show(local)
+
+	local = sanitize(incrByStr(local, "NOPE"))
+	show(local)
+
+	local = limit(incrBy(local, 5), 500)
+	show(local)
+}
+
+func show(n int) {
+	fmt.Printf("show => n = %d\n", n)
+}
+
+func incrWrong(n int) {
+	n++
+}
+
+func incr(n int) int {
+	n++
+	return n
+}
+
+func incrBy(n, factor int) int {
+	return n * factor
+}
+
+func incrByStr(n int, factor string) (int, error) {
+	m, err := strconv.Atoi(factor)
+	n = incrBy(n, m)
+	return n, err
+}
+
+func sanitize(n int, err error) int {
+	if err != nil {
+		return 0
+	}
+	return n
+}
+
+func limit(n, lim int) (m int) {
+	m = n
+	if m > lim {
+		m = lim
+	}
+	return m
+}

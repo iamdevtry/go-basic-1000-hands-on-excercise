e60b7b10e96795a594b53e06c3928a3046593394
iamdevtry <tranbinhphongcmg@gmail.com>
2022-11-17 19:57:58 +0700

    done method lesson

---
+++ SECTION_14/182_methods/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	mobydick := book{
+		title: "moby dick",
+		price: 10,
+	}
+
+	minecraft := game{
+		title: "minecraft",
+		price: 20,
+	}
+
+	mobydick.print()
+	minecraft.print()
+}

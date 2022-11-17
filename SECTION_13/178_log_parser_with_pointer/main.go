e92aeab30fd5ba1cb274eec03741a59809ed9b9b
iamdevtry <tranbinhphongcmg@gmail.com>
2022-11-17 16:16:41 +0700

    done lesson 178

---
+++ SECTION_13/178_log_parser_with_pointer/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+)
+
+func main() {
+	p := newParser()
+
+	in := bufio.NewScanner(os.Stdin)
+
+	for in.Scan() {
+		p.lines++
+
+		parsed, err := parse(p, in.Text())
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		update(&p, parsed)
+		fmt.Printf("main.parser after 	:\n\t%+v\n", p)
+	}
+
+	sort.Strings(p.domains)
+
+	fmt.Printf("%-30s %10s\n", "DOMAIN", "VISITS")
+	fmt.Println(strings.Repeat("-", 45))
+}
